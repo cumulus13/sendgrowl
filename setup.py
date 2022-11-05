34f0435c3c29317e552f2060363a322673630be8
cumulus13 <cumulus13@gmail.com>
2022-11-05 07:33:05 +0700

    version: 0.54 ~ 2022-11-05 07:33:02:588324

--- setup.py
+++ setup.py
@@ -1,43 +1,110 @@
-from setuptools import setup, find_packages
+from __future__ import print_function
+from setuptools import setup
+
+import os, sys
+import shutil
+
+NAME = "sendgrowl"
+
+def get_version():
+    """Get version and version_info without importing the entire module."""
+    print("NAME:", NAME)
+    path = os.path.join(os.path.dirname(__file__), NAME, '__meta__.py')
+
+    if sys.version_info.major == 3:
+        import importlib.util
+
+        spec = importlib.util.spec_from_file_location("__meta__", path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        vi = module.__version_info__
+        return vi._get_canonical(), vi._get_dev_status()
+    else:
+        import imp
+        vi = imp.load_source("meat", "__meta__.py")
+        return vi.__version__, vi.__status__
+
+def get_requirements(req):
+    """Load list of dependencies."""
+
+    install_requires = []
+    with open(req) as f:
+        for line in f:
+            if not line.startswith("#"):
+                install_requires.append(line.strip())
+    return install_requires
+
+
+def get_description():
+    """Get long description."""
+
+    desc = ''
+
+    if os.path.isfile('README.md'):
+        with open("README.md", 'r') as f:
+            desc = f.read()
+    return desc
+
+VER, DEVSTATUS = get_version()
+
+try:
+    os.remove(os.path.join(NAME, '__version__.py'))
+except:
+    pass
+try:
+    os.remove(os.path.join(NAME, 'sendgrowl.py'))
+except:
+    pass
+shutil.copy2('__version__.py', NAME)
+shutil.copy2('sendgrowl.py', NAME)
+
 import __version__
 version = __version__.version
+
+if sys.version_info.major == 3:
+    entry_points = {
+         "console_scripts": ["sendgrowl3 = sendgrowl:usage",]
+    },
+else:
+    entry_points = {
+         "console_scripts": ["sendgrowl = sendgrowl:usage",]
+    },    
+
 setup(
-    name = 'sendgrowl',
-    version = version,
+    name=NAME,
+    version=version,
     author = 'Hadi Cahyadi LD',
     author_email = 'cumulus13@gmail.com',
+    maintainer="cumulus13 Team",
+    maintainer_email="cumulus13@gmail.com",    
     description = ('simple send notify to growl'),
     license = 'MIT',
-    keywords = "youtube downloader download channel",
+    keywords = "growl windows linux gntp gntplib 23053",
     url = 'https://github.com/cumulus13/sendgrowl',
-    scripts = [],
-    py_modules = ['sendgrowl'],
-    packages = find_packages(),
+    project_urls={
+        "Documentation": "https://github.com/cumulus13/sendgrowl",
+        "Code": "https://github.com/cumulus13/sendgrowl",
+    },
+    packages = ['sendgrowl'],
     download_url = 'https://github.com/cumulus13/sendgrowl/tarball/master',
     install_requires=[
         'gntplib',
-        'argparse'
+        'argparse',
+        'configset'
     ],
-    # TODO
-    #entry_points={
-    #    "console_scripts": ["drawille=drawille:__main__"]
-    #},
-    entry_points = {
-         "console_scripts": ["sendgrowl = sendgrowl:usage",]
-    },
-    classifiers = [
-        "Development Status :: 4 - Beta",
-        "Topic :: Utilities",
+    entry_points = entry_points,
+    data_files=['__version__.py', 'growl.png', 'sendgrowl.ini'],
+    include_package_data=True,
+    python_requires=">=2.7",    
+    classifiers=[
+        'Development Status :: %s' % DEVSTATUS,
         'Environment :: Console',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
