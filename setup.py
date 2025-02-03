ffe259f8aaf455faa77541795a1db1f9820d1528
cumulus13 <cumulus13@gmail.com>
2025-02-03 10:12:46 +0700

    version: 0.73 ~ 2025-02-03 10:12:45:301052

--- setup.py
+++ setup.py
@@ -101,4 +101,6 @@ setup(
         "Programming Language :: Python :: 3",
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
+    long_description=get_description(),
+    long_description_content_type="text/markdown",
 )
